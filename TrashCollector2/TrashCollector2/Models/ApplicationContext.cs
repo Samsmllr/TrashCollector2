ca1a532c2f7541daaa223e874bed5bb2cdd3ad4b
samsmllr <sam.s.mllr@gmail.com>
2020-02-27 15:23:11 -0600

    Added User Model, fleshed out database form.

---
+++ TrashCollector2/TrashCollector2/Models/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector2.Models
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+        
+    }
+}
---
+++ TrashCollector2/TrashCollector2/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector2.Models
+{
+
+public class User : IdentityUser
+    {
+        [Display(Name = "Name")]
+        [MaxLength (50)]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Address")]
+        [MaxLength(100)]
+        [Required]
+        public string Address { get; set; }
+        
+        [Display(Name = "Address Two")]
+        [MaxLength(100)]
+        public string AddressTwo { get; set; }
+
+        [Display(Name = "State")]
+        [Required]
+        public string StateIs { get; set; }
+
+        [Display(Name = "ZipCode")]
+        [Required]
+        public string Zip { get; set; }
+
+    }
+}
